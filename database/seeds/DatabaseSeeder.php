c35d4e9ceb0ebabd24ce5289e19f601c8998eda7
kalynrobinson <kalyn.robinson@rtslabs.com>
2017-11-29 17:16:58 -0500

    Update .env.example and seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,15 +15,12 @@ class DatabaseSeeder extends Seeder
         factory(App\User::class, 1)->create([
             'name' => env('ADMIN_NAME'),
             'email' => env('ADMIN_EMAIL'),
-            'password' => env('ADMIN_PASSWORD')
+            'password' => bcrypt(env('ADMIN_PASSWORD'))
         ]);
-
-        // Create users
-        factory(App\User::class, 20)->create();
 
         // Creates initial API key
         DB::connection('flarum')->table('api_keys')->insert([
-            'id' => env('FLARUM_API_KEY')
+            'id' => config('flarum.api_key')
         ]);
     }
 }
